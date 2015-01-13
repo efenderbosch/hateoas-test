7788673dbb1e96348b3ed3ba296f7b6ad5170421
Eric Fenderbosch <eric.fenderbosch@segmint.com>
2015-01-13 13:36:01 -0500

    removed JpaSpecificationExecutor

--- src/main/java/com/example/ChildRepository.java
+++ src/main/java/com/example/ChildRepository.java
@@ -1,8 +1,7 @@
 package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ChildRepository extends JpaRepository<Child, Long>, JpaSpecificationExecutor<Child> {
+public interface ChildRepository extends JpaRepository<Child, Long> {
 
 }
